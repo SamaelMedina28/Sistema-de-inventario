16f4d7fc4febee879d62192098cecbbf917285b1
Samael Home <samaelortiz2218@gmail.com>
2025-06-20 21:04:58 -0700

    Items sidebar dinámico

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -1,28 +1,108 @@
-<?php
+@php
     $links = [
-        'dashboard' => [
-            'icon' => 'fas fa-home',
-            'label' => 'Dashboard',
-            'route' => route('admin.dashboard'),
+        [
+            'header' => 'Principal',
         ],
+        [
+            'name' => 'Dashboard',
+            'icon' => 'fas fa-home',
+            'route' => route('admin.dashboard'),
+            'active' => request()->routeIs('admin.dashboard'),
+            'submenu' => [
+                'items' => [
+                    [
+                        'name' => 'Enlace 1',
+                        'route' => '#',
+                    ],
+                    [
+                        'name' => 'Enlace 2',
+                        'route' => '#',
+                    ],
+                    [
+                        'name' => 'Enlace 3',
+                        'route' => '#',
+                    ],
+                ],
+            ],
+        ],
+        // [
+        //     'header' => 'Management',
+        // ],
+        // [
+        //     'name' => 'Orders',
+        //     'icon' => 'fas fa-shopping-cart',
+        //     'route' => route('admin.dashboard'),
+        //     'active' => request()->routeIs('admin.orders.*'),
+        // ],
+        // [
+        //     'name' => 'Users',
+        //     'icon' => 'fas fa-users',
+        //     'route' => route('admin.dashboard'),
+        //     'active' => request()->routeIs('admin.users'),
+        // ],
+        // [
+        //     'name' => 'Roles',
+        //     'icon' => 'fas fa-user-tag',
+        //     'route' => route('admin.dashboard'),
+        //     'active' => request()->routeIs('admin.roles.*'),
+        // ],
+        // [
+        //     'name' => 'Settings',
+        //     'icon' => 'fas fa-cog',
+        //     'route' => route('admin.dashboard'),
+        //     'active' => request()->routeIs('admin.settings.*'),
+        // ],
     ];
-?>
+@endphp
 <aside id="logo-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
     aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white ">
         <ul class="space-y-2 font-medium">
             @foreach ($links as $link)
-            <li>
-                <a href="{{ $link['route'] }}" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-                    <span class=" inline-flex justify-center items-center text-gray-500">
-                        <i class="{{ $link['icon'] }}"></i>
-                    </span>
-                    <span class="ms-3">{{ $link['label'] }}</span>
-                </a>
-            </li>
-            @endforeach
+                <li>
+                    @isset($link['header'])
+                        <div class="px-3 py-1 text-xs font-semibold text-gray-500 uppercase">
+                            {{ $link['header'] }}
+                        </div>
+                    @else
+                        @isset($link['submenu'])
+                            <button type="button"
+                                class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100"
+                                aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                                <span class="inline-flex justify-center items-center text-gray-500">
+                                    <i class="{{ $link['icon'] }}"></i>
+                                </span>
+                                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ $link['name'] }}</span>
+                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="m1 1 4 4 4-4" />
+                                </svg>
+                            </button>
+                            <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                                @foreach ($link['submenu']['items'] as $item)
+                                    <li>
+                                        <a href="{{ $item['route'] }}"
+                                            class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100">
+                                            {{ $item['name'] }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
 
+                        @else
+                            <a href="{{ $link['route'] }}"
+                                class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 {{ $link['active'] ? 'bg-gray-100' : '' }}">
+                                <span class="inline-flex justify-center items-center text-gray-500">
+                                    <i class="{{ $link['icon'] }}"></i>
+                                </span>
+                                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ $link['name'] }}</span>
+                            </a>
+                        @endisset
+                    @endisset
+                </li>
+            @endforeach
         </ul>
     </div>
 </aside>
