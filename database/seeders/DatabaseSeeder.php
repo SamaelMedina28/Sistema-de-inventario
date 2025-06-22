97bc076b3fdb6167d979a20b8e9dd157fe2cc8ad
Samael Home <samaelortiz2218@gmail.com>
2025-06-22 14:51:49 -0700

    creando productos de prueba con factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\CategorySeeder;
@@ -25,5 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
         ]);
+
+        Product::factory(100)->create();
     }
 }
