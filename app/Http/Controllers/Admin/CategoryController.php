d6246699b57693243aa04658d7a47ab01d52c1b4
Samael Home <samaelortiz2218@gmail.com>
2025-06-22 12:08:28 -0700

    Crear nueva categoria

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -29,7 +29,14 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name',
+            'description' => 'nullable|string',
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('admin.categories.index');
     }
 
     /**
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,3 +1,17 @@
 <x-admin-layout :breadcrumbs="[['name' => 'Dashboard', 'href' => route('admin.dashboard')], ['name' => 'Categories', 'href' => route('admin.categories.index')], ['name' => 'Create']]" title='Category'>
-    
+    <x-wireui-card>
+        <form action="{{ route('admin.categories.store') }}" method="POST" class="space-y-4">
+            @csrf
+            <x-wireui-input name="name" label="Name" placeholder="Category name" value="{{ old('name') }}"/>
+            <x-wireui-textarea name="description" label="Description" placeholder="Category description">
+                {{ old('description') }}
+            </x-wireui-textarea>
+            <div class="flex justify-end space-x-2">
+                <x-wireui-button flat negative label="Cancel" href="{{ route('admin.categories.index') }}" />
+                <x-button type="submit">
+                    Create
+                </x-button>
+            </div>
+        </form>
+    </x-wireui-card>
 </x-admin-layout>
